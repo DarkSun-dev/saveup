d779d89aba310d728c7ef38271a75ded26d97244
DarkSun-dev <juniorvicenty2000@gmail.com>
2024-04-03 17:00:16 +0200

    first commit

--- models/alertModel.js
+++ models/alertModel.js
@@ -1,7 +1,9 @@
 const mongoose = require('mongoose');
 const alertSchema = new mongoose.Schema({
+    entidades: Array,
     user: Object,
-    message: String
+    message: String,
+    status: Object
 })
 const Alert = mongoose.model('Alert', alertSchema);
 module.exports = Alert
